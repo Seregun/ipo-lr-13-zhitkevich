656d5436c422f0d1c0d04942ee8fa9418ed52a15
Seregun <seregunmgp@gmail.com>
2024-12-21 08:53:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from ForImages.image_handler import ImageHandler
 
 def main():
-    image_path = input("Enter the path to the image (e.g., images/image.jpg): ")
+    image_path = input("Введите путь к изображению (Images\\image.jpg): ")
     try:
         handler = ImageHandler(image_path)
 
@@ -14,9 +14,9 @@ def main():
 
         processor.image.show()
         processor.image.save("processed_image.png")
-        print("Processed image saved as 'processed_image.png'.")
+        print("Обработанное изображение, сохраненное как 'processed_image.png'.")
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"Произошла ошибка: {e}")
 
 if __name__ == "__main__":
     main()
